208301c90b2acf3e5b926f9f03c11ce6593b0b31
MaykeBolt <maykesouza2805@gmail.com>
2023-10-25 22:18:45 -0300

    Tirando o 'index.html' da pasta 'public'.

--- BooPeep-web/src/screens/Recovery/Login_Recovery.tsx
+++ BooPeep-web/src/screens/Recovery/Login_Recovery.tsx
@@ -39,10 +39,7 @@ function Login_Recovery() {
                             focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
                             dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" /*required=""*/ />
 
-                            <a href="login" type="submit" className="w-full text-black hover:bg-purple-600 
-                            focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 
-                            py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 
-                            dark:focus:ring-primary-800 bg-purple-500 mt-3"> Enviar </a>                     
+                            <a href="login" type="submit" className="w-full text-black hover:bg-purple-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 bg-purple-500 mt-3"> Enviar </a>                     
                         </div>
                     </form>
                 </div>
